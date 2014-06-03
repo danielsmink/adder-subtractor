510a4667af4e9d7cb097ae4fe1cdf9a46b247fc3
danielsmink <smink83@gmail.com>
2014-06-03 19:55:21 +0200

    Update main.js
    Changed some ids and variable names

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 var x = document.getElementById('xInput');
 var y = document.getElementById('yInput');
 var msg = document.getElementById('msg');
-var plusMinus = document.getElementById('plusMinus');
-var answer = document.getElementById('result');
-var answer2 = document.getElementById('result2');
+var result = document.getElementById('result');
+var result2 = document.getElementById('result2');
 
 function xOut(input){
   var xInput = input.value;
-  document.getElementById('xOutSpan').innerHTML = document.getElementById('xOutSpan2').innerHTML = xInput;
+  document.getElementById('sum1X').innerHTML = document.getElementById('sum2X').innerHTML = xInput;
 
   if(xInput.length <= 0) {
-    document.getElementById('xOutSpan').innerHTML = document.getElementById('xOutSpan2').innerHTML = 'X';
+    document.getElementById('sum1X').innerHTML = document.getElementById('sum2X').innerHTML = 'X';
   }
   validate(xInput, x);
 }
 
 function yOut(input){
   var yInput = input.value;
-  document.getElementById('yOutSpan').innerHTML = document.getElementById('yOutSpan2').innerHTML = yInput;
+  document.getElementById('sum1Y').innerHTML = document.getElementById('sum2Y').innerHTML = yInput;
 
   if(yInput.length <= 0) {
-    document.getElementById('yOutSpan').innerHTML = document.getElementById('yOutSpan2').innerHTML = 'Y';
+    document.getElementById('sum1Y').innerHTML = document.getElementById('sum2Y').innerHTML = 'Y';
   }
   validate(yInput, y);
 }
@@ -29,7 +28,7 @@ function validate(input, element){
   if(input.length >= 0 && isNaN(input) || input % 1 !== 0) {  
     element.setAttribute("class", "error");
     msg.innerHTML = "The value must be an integer.";
-    answer.innerHTML = answer2.innerHTML = 'Z';
+    result.innerHTML = result2.innerHTML = 'Z';
   } else {
     element.className = '';
     msg.innerHTML ='';
@@ -41,9 +40,9 @@ function calc(){
   var yInput = y.value;
 
   if(xInput && yInput){
-    answer.innerHTML = parseInt(xInput) + parseInt(yInput);
-    answer2.innerHTML = parseInt(xInput) - parseInt(yInput);
+    result.innerHTML = parseInt(xInput) + parseInt(yInput);
+    result2.innerHTML = parseInt(xInput) - parseInt(yInput);
   } else {
-    answer.innerHTML = answer2.innerHTML = 'Z';
+    result.innerHTML = result2.innerHTML = 'Z';
   }
 }
