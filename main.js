874ad53f803a26a68e3bd408b49074cf106fc5a9
danielsmink <smink83@gmail.com>
2014-06-03 20:20:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-var x = document.getElementById('xInput');
-var y = document.getElementById('yInput');
-var msg = document.getElementById('msg');
-var result1 = document.getElementById('result1');
-var result2 = document.getElementById('result2');
+var x = document.getElementById('xInput'),
+    y = document.getElementById('yInput'),
+    msg = document.getElementById('msg'),
+    result1 = document.getElementById('result1'),
+    result2 = document.getElementById('result2'),
+    xInput,
+    yInput;
 
 function xOut(input){
-  var xInput = input.value;
+  xInput = input.value;
+  
   document.getElementById('sum1X').innerHTML = document.getElementById('sum2X').innerHTML = xInput;
 
   if(xInput.length <= 0) {
@@ -15,7 +18,8 @@ function xOut(input){
 }
 
 function yOut(input){
-  var yInput = input.value;
+  yInput = input.value;
+  
   document.getElementById('sum1Y').innerHTML = document.getElementById('sum2Y').innerHTML = yInput;
 
   if(yInput.length <= 0) {
@@ -25,8 +29,10 @@ function yOut(input){
 }
 
 function validate(input, element){
-  var element1 = element;
-  var input1 = input;
+  var element1 = element,
+      input1 = input
+      element2,
+      input2;
   
   if(element1.id == 'xInput') {
     element2 = y;
@@ -39,6 +45,7 @@ function validate(input, element){
   if(input1.length >= 0 && isNaN(input1) || input1 % 1 !== 0 || isNaN(input2) || input2 % 1 !== 0) {  
     element1.setAttribute("class", "error");
     msg.innerHTML = "The value must be an integer.";
+
     result1.innerHTML = result2.innerHTML = 'Z';
   } else {
     element1.className = element2.className = '';
@@ -47,8 +54,12 @@ function validate(input, element){
 }
 
 function calc(){
-  var xInput = x.value;
-  var yInput = y.value;
+  if(x.className != 'error') {
+    xInput = x.value;
+  }
+  if(y.className != 'error') {
+    yInput = y.value;
+  }
 
   if(xInput && yInput){
     result1.innerHTML = parseInt(xInput) + parseInt(yInput);
